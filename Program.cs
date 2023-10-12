24fff6d695937c4b17dcb7033027f45959279a3d
bilalakuji <113610858+bilalakuji@users.noreply.github.com>
2023-10-12 18:29:43 +0100

    Created textbox for name badge

--- Program.cs
+++ Program.cs
@@ -1,24 +1,35 @@
 ﻿using System;
-
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Spectre.Console;
 namespace programmingproblems
 {
     class NameBadgeGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, please input your name:");
+
+            AnsiConsole.Write(
+                new FigletText("Hello")
+                    .Color(Color.Blue));
+
+            AnsiConsole.Markup("[underline red]Hello[/], please input your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine($"Hi {name}!");
+            AnsiConsole.Markup($"Hi [underline blue]{name}[/]! ");
 
             Console.WriteLine("Please now enter your job occupation:");
             string job = Console.ReadLine();
+            AnsiConsole.Write(
+                new FigletText("Wow!")
+                    .Color(Color.Yellow));
 
             Console.WriteLine("Next,enter your phone number");
-            double number = Convert.ToDouble(Console.ReadLine());
+            decimal number = Convert.ToDecimal(Console.ReadLine());
 
             Console.WriteLine("Please see your name badge below");
-
-            Console.WriteLine($"\nName: {name}\nOccupation: {job}\nContact Number: {number}");
+                Console.WriteLine($"┌───────────────────────────────────────────────────────────────────-─┐");
+                Console.WriteLine($"│ Name:           {name}\n  Occupation:     {job}\n  Contact Number: {number}");
+                Console.WriteLine($"└─────────────────────────────────────────────────────────────────────┘");
         }
     }
 }
